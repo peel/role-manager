5414195067a52c311e28fc78e353f66d252ad9ea
peel <plimanowski@gmail.com>
2016-03-07 16:40:48 +0100

    More typesafe role handling

--- es_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 2113:2113 -p 1113:1113 adbrowne/eventstore
--- src/main/scala/JsonProtocols.scala
+++ src/main/scala/JsonProtocols.scala
@@ -16,13 +16,13 @@ trait JsonProtocols extends DefaultJsonProtocol {
   implicit object UnsubscribedFormat extends JsonFormat[Unsubscribed]{
     override def write(evt: Unsubscribed) = JsObject(
       ("date", JsString(evt.time.format(DateTimeFormatter.ISO_ZONED_DATE_TIME))),
-      ("role", JsString("false")))
+      ("role", JsString(Role.Unsubscribed.toString)))
     override def read(json: JsValue) = ??? //not used
   }
   implicit object SubscribedFormat extends JsonFormat[Subscribed]{
     override def write(evt: Subscribed) = JsObject(
       ("date", JsString(evt.time.format(DateTimeFormatter.ISO_ZONED_DATE_TIME))),
-      ("role", JsString("true")))
+      ("role", JsString(Role.Subscribed.toString)))
     override def read(json: JsValue) = ??? //not used
   }
   implicit object EventFormat extends JsonFormat[Event]{
@@ -31,11 +31,17 @@ trait JsonProtocols extends DefaultJsonProtocol {
       case u: Unsubscribed => u.toJson
     }
     override def read(json: JsValue) = json.asJsObject.getFields("date", "role") match {
-      case Seq(JsString(date),JsString(role)) if role == "true" => Subscribed(ZonedDateTime.parse(date, DateTimeFormatter.ISO_ZONED_DATE_TIME))
-      case Seq(JsString(date),JsString(role)) if role != "true" => Unsubscribed(ZonedDateTime.parse(date, DateTimeFormatter.ISO_ZONED_DATE_TIME))
-      case err => deserializationError(s"Event expected but not found: $err")
+      case Seq(JsString(date),JsString(role)) if role == Role.Subscribed.toString => Subscribed(ZonedDateTime.parse(date, DateTimeFormatter.ISO_ZONED_DATE_TIME))
+      case Seq(JsString(date),JsString(role)) if role != Role.Unsubscribed.toString => Unsubscribed(ZonedDateTime.parse(date, DateTimeFormatter.ISO_ZONED_DATE_TIME))
+      case err => deserializationError(s"Event expected but found: $err")
     }
   }
   implicit val roleStateFormat = jsonFormat1(UserRoleState.apply)
   implicit val roleChangeFormat = jsonFormat1(UserRoleChange.apply)
+}
+
+object Role extends Enumeration{
+  type Role = Value
+  val Subscribed = Value("subscribed")
+  val Unsubscribed = Value("unsubscribed")
 }
